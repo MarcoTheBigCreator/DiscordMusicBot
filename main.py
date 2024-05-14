1c5b788d1a6dfa1e53ce29dfa9851713980cb723
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-13 22:32:20 -0600

    refactor: Update Now Playing field in queue embed

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ async def queue(ctx):
                     requester_mention = "Unknown"
                 
                 if i == 0 and ctx.voice_client.is_playing():
-                    embed.add_field(name=f"🎶 {i + 1}. {item[0]['title']}", value=f"Requested by: {requester_mention} - **Now Playing**", inline=False)
+                    embed.add_field(name=f"**Now Playing - ** {i + 1}. {item[0]['title']}", value=f"Requested by: {requester_mention}", inline=False)
                 else:
                     embed.add_field(name=f"{i + 1}. {item[0]['title']}", value=f"Requested by: {requester_mention}", inline=False)
             if len(mqueue) > max_songs:

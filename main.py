e360b78ffc8bac3a15ec6c6b9d1db2eb0cd283e7
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-13 22:35:33 -0600

    refactor: Update Now Playing field in queue embed

--- main.py
+++ main.py
@@ -201,9 +201,9 @@ async def queue(ctx):
                     requester_mention = "Unknown"
                 
                 if i == 0 and ctx.voice_client.is_playing():
-                    embed.add_field(name=f"**Now Playing - ** {i + 1}. {item[0]['title']}", value="", inline=True)
+                    embed.add_field(name=f"**Now Playing - ** {i + 1}. {item[0]['title']}", value="", inline=False)
                 else:
-                    embed.add_field(name=f"{i + 1}. {item[0]['title']}", value="", inline=True)
+                    embed.add_field(name=f"{i + 1}. {item[0]['title']}", value="", inline=False)
             if len(mqueue) > max_songs:
                 embed.set_footer(text=f"And {len(mqueue) - max_songs} more...")
         else:
